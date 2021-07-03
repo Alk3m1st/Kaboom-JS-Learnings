90b4afd137751ed4c017a4d2ecb5d366e275f818
Allan Oliveira <r9oliveira@gmail.com>
2021-07-03 16:46:23 +0800

    Added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     game: './src/game.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Mario Kaboom JS',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
