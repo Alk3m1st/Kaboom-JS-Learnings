8701cd7fa9e477903dfdd6697e2c38e3b08f58f2
Allan Oliveira <r9oliveira@gmail.com>
2021-07-12 19:01:23 +0800

    Fix live reload and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { transpileModule } = require("typescript");
 
 module.exports = {
   mode: "development", // Not for prod obviously
   devtool: "inline-source-map",
   devServer: {
-    contentBase: ".dist",
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
+    //publicPath: path.join(__dirname, "dist"),
+    port: 8888,
+    compress: true,
     hot: true,
+    watchOptions: {
+      aggregateTimeout: 200,
+      ignored: "**/node_modules",
+      poll: 1000,
+    },
   },
   entry: {
     game: "./src/game.ts",
