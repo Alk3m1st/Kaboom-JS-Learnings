1dcc9d146b61803fa29f46c4e67f9be3838eeb2e
Allan Oliveira <r9oliveira@gmail.com>
2021-07-17 21:18:19 +0800

    Start splitting out constants into separate files.

---
+++ src/constants/labels.ts
@@ -0,0 +1,4 @@
+export const ENEMY: string = "enemy";
+export const HEADBUMP: string = "headbump";
+export const COIN_SURPRISE: string = "coin-surprise";
+export const MUSHROOM_SURPRISE: string = "mushroom-surprise";
