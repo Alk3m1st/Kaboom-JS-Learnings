dc1302e2330e6c494ec29e93ff65f122ba510641
Allan Oliveira <r9oliveira@gmail.com>
2021-07-25 18:42:36 +0800

    Added some additional labels to constants

--- src/constants/labels.ts
+++ src/constants/labels.ts
@@ -1,4 +1,6 @@
 export const ENEMY: string = "enemy";
 export const HEADBUMP: string = "headbump";
+export const COIN: string = "coin";
 export const COIN_SURPRISE: string = "coin-surprise";
+export const MUSHROOM: string = "mushroom";
 export const MUSHROOM_SURPRISE: string = "mushroom-surprise";
